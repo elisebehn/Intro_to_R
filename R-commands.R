822d56a0ebfaf65af39f813365f3d52e058b871e
Behn <beh028@csiro.au>
2020-03-12 13:19:46 +1100

    changes to R commands

---
+++ R-commands.R
@@ -0,0 +1,52 @@
+1+1
+X <- 6
+
+X
+
+X <- 5+7
+X
+
+X <- X+2
+X
+
+Y <- X *2
+Y
+
+X <- 2
+X
+Y
+NA
+
+x <- NA
+
+2+X
+intro <- "Hi!"
+
+3==3
+3+3
+3=3
+
+Y == 3
+Y==3
+
+4!= 3
+
+5>= Y
+
+
+
+
+
+
+mass <- 47.5
+
+age <- 122
+
+mass <- mass*2.3
+
+age <- age - 20
+
+
+mass >= age
+
+some_numbers <- seq(1, 10)
