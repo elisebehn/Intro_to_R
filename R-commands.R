eadf559179bc09543fc0c0bb9d0eb643e2cc2bca
Behn <beh028@csiro.au>
2020-03-12 14:16:42 +1100

    playing with filters

--- R-commands.R
+++ R-commands.R
@@ -19,4 +19,24 @@ cutdown_gapminder <- select(gapminder,-pop,-year)
 
 select(gapminder, country, year,pop, gdpPercap)
 select(gapminder,-pop,-lifeExp)
-select(gapminder, 1,3,4,5)
+select(gapminder, 1,3,5,6)
+
+
+#renaming variables = rename(new = old)
+rename(gapminder,country_name = country)
+
+rename(gapminder, life = lifeExp, cap = gdpPercap)
+
+
+# column functions 
+#Select & Rename
+
+#filter (== to include , != to exclude, greater Than> less than<)
+
+filter(gapminder, country== "Australia")
+
+filter(gapminder, year>=1997)
+
+
+filter(gapminder, lifeExp >80)
+filter(gapminder,continent == "Europe")
